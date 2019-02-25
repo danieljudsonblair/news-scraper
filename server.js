9c4b10abbd1aadd9f7aabe10caccd3f1c5fb6611
s1191 <danieljudsonblair@gmail.com>
2019-02-25 09:56:18 -0500

    logging

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+console.log(MoNGODB_URI);
 
 mongoose.connect(MONGODB_URI);
 
