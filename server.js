ad0ab9a91e6a8f24dd65ce51021acfcd18647018
s1191 <danieljudsonblair@gmail.com>
2019-02-25 10:01:11 -0500

    changed db line

--- server.js
+++ server.js
@@ -32,9 +32,8 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/scraper");
 
 // Routes
 
